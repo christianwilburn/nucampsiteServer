8f29f27c1bd59beeed336a9eeba94e17a35e6e55
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2022-07-08 22:00:30 -0400

    passport facebook

--- authenticate.js
+++ authenticate.js
@@ -1,16 +1,10 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("./models/user");
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 
@@ -46,6 +40,37 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', { session: false});
+
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+      {
+          clientID: config.facebook.clientId,
+          clientSecret: config.facebook.clientSecret
+      }, 
+      (accessToken, refreshToken, profile, done) => {
+          User.findOne({facebookId: profile.id}, (err, user) => {
+              if (err) {
+                  return done(err, false);
+              }
+              if (!err && user) {
+                  return done(null, user);
+              } else {
+                  user = new User({ username: profile.displayName });
+                  user.facebookId = profile.id;
+                  user.firstname = profile.name.givenName;
+                  user.lastname = profile.name.familyName;
+                  user.save((err, user) => {
+                      if (err) {
+                          return done(err, false);
+                      } else {
+                          return done(null, user);
+                      }
+                  });
+              }
+          });
+      }
+  )
+);
 
 exports.verifyAdmin = function (req, res, next) {
   if(req.user.admin === true) {
